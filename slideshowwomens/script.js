e3e0101896e7efb15d051af6f473260501734c0c
Caramello Marcel Arnold <140120013+caramellocode@users.noreply.github.com>
2023-09-24 13:30:18 +0200

    Add files via upload

---
+++ slideshowwomens/script.js
@@ -0,0 +1,17 @@
+function hideImage1(){
+    document.getElementById('image2').classList.remove('d-none');
+    document.getElementById('image3').classList.remove('d-none');
+
+
+}
+
+
+function hideImage2(){
+    document.getElementById('image2').classList.add('d-none');
+
+}
+
+
+function hideImage3(){
+    document.getElementById('image3').classList.add('d-none');
+}
